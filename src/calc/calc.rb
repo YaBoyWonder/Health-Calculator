7aaae70a7c21005d695b9184e955a7c65127a766
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2018-08-24 22:32:25 -0700

    hlth_lb

--- src/calc/calc.rb
+++ src/calc/calc.rb
@@ -2,30 +2,28 @@ module Calc
  class HLTH  
 
 	   def pound_to_kilo_converter(weight_lb)
-	      @weight_kg = ( weight_lb * (0.453592)  )
+	      @weight_kg = ( weight_lb * (0.452873)  )
 	   	  print @weight_kg      
 	   end
 
 
 	   def kilo_to_pound_converter(weight_kg)
 	   	  puts "#{weight_kg} Kilos"
-	      @weight_lb =   ( (weight_kg)  * (0.453592)  ).to_f + "\s Pounds"
-	   end
-
-  
-	   def inch_to_feet_converter
-	      
-	   end
-
-
+	      @weight_lb =   ( (weight_kg)  * (0.452873)  ).to_f + "\s Pounds"
+       end
+       
 	 
-	  def bmi_kg(weight_kg,height_mi)
+	  def hlth_kg(weight_kg,height_mi)
 	  	puts "Weight should be in Kilos and Height should be in meters"
 	  
-	    @bmi_calac_kg = (weight_kg / (height_mi * height_mi)).round(2)
+	    @HLTH_calac_kg = (weight_kg / (height_mi * height_mi)).round(2)
 	  end
 
-
+    
+      def hlth_lb(weight_lb,height_in)
+        puts "Weight should be in Pounds and Height in Inches "
+         @hlth_calac_lb = ( (weight_lb) / (height_in * height_in).round(2) ) * 703.06927748
+     end
   
   
   
